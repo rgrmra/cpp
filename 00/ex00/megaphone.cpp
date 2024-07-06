4cda38636e0a57e1cebd5dad8928faf178e1b694
Roger de Moura <rde-mour@student.42sp.org.br>
2024-07-06 08:44:36 -0300

    refactor: Simplify megaphone logic

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,14 +6,11 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*    Created: 2024/07/01 17:31:36 by rde-mour          #+#    #+#            */
-/*   Updated: 2024/07/01 22:08:31 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/07/06 08:43:50 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <algorithm>
-#include <cctype>
-#include <cmath>
-#include <cstdlib>
 #include <iostream>
 
 static std::string	megaphone(std::string str)
@@ -24,9 +21,9 @@ static std::string	megaphone(std::string str)
 
 int	main(int argc, char **argv)
 {
-	if (std::isless(argc, 2))
+	if (argc < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i = 1; std::isless(i, argc); ++i)
+	for (int i = 1; i < argc; ++i)
 		std::cout << megaphone(argv[i]);
 	std::cout << std::endl;
 	return (EXIT_SUCCESS);
