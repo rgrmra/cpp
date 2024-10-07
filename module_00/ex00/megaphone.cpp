28aec5eafec8bcc7614a212971a54c1d92e44e36
Roger de Moura <rde-mour@student.42sp.org.br>
2024-10-07 18:59:42 -0300

    fix: remove algorithm and improve code

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,25 +6,29 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*    Created: 2024/07/01 17:31:36 by rde-mour          #+#    #+#            */
-/*   Updated: 2024/07/06 08:43:50 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/10/07 18:59:07 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <algorithm>
+#include <cstdlib>
 #include <iostream>
-
-static std::string	megaphone(std::string str)
-{
-	std::transform(str.begin(), str.end(), str.begin(), toupper);
-	return str;
-}
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2)
+	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i = 1; i < argc; ++i)
-		std::cout << megaphone(argv[i]);
+
+	for (int i = 1; i < argc; i++)
+	{
+		std::string	str = argv[i];
+
+		for (size_t j = 0; j < str.length(); j++)
+			str.at(j) = static_cast<unsigned char>(toupper(str.at(j)));
+
+		std::cout << str;
+	}
+
 	std::cout << std::endl;
+
 	return (EXIT_SUCCESS);
 }
