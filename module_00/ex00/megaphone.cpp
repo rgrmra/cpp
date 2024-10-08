2304ec4f63a7edfe04b28e70d8dcd6906bb4c099
Roger de Moura <rde-mour@student.42sp.org.br>
2024-10-08 15:41:42 -0300

    refact: cpp code conventions

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*    Created: 2024/07/01 17:31:36 by rde-mour          #+#    #+#            */
-/*   Updated: 2024/10/07 19:04:51 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/10/08 15:38:47 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,10 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 
+	std::string str;
 	for (int i = 1; i < argc; i++)
 	{
-		std::string	str = argv[i];
+		str = argv[i];
 
 		for (size_t j = 0; j < str.length(); j++)
 			str.at(j) = static_cast<unsigned char>(toupper(str.at(j)));
@@ -31,5 +32,5 @@ int	main(int argc, char **argv)
 
 	std::cout << std::endl;
 
-	return (EXIT_SUCCESS);
+	return EXIT_SUCCESS;
 }
