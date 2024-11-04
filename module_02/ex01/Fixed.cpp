c6c781f37e5e925207195a95be5ea474c5d1fb5f
Roger de Moura <rde-mour@student.42sp.org.br>
2024-11-04 18:59:11 -0300

    chore: change functions output

--- module_02/ex01/Fixed.cpp
+++ module_02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 19:24:34 by rde-mour          #+#    #+#             */
-/*   Updated: 2024/11/04 12:54:52 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/11/04 18:58:29 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ Fixed& Fixed::operator=(const Fixed &rhs)
 	if (this == &rhs)
 		return *this;
 
-	_value = rhs._value;
+	_value = rhs.getRawBits();
 
 	return *this;
 }
@@ -71,7 +71,7 @@ Fixed::~Fixed(void)
 int Fixed::getRawBits(void) const
 {
 	std::cout
-		<< "getRawBits function called"
+		<< "getRawBits member function called"
 		<< std::endl;
 
 	return _value;;
@@ -80,7 +80,7 @@ int Fixed::getRawBits(void) const
 void Fixed::setRawBits(int const raw)
 {
 	std::cout
-		<< "setRawBits function called"
+		<< "setRawBits member function called"
 		<< std::endl;
 
 	_value = raw;
