bc71ee76e299b329e7534ae23014ec24a3217a42
Roger de Moura <rde-mour@student.42sp.org.br>
2024-12-11 20:48:54 -0300

    refactor: change code

--- module_06/ex00/ScalarConverter.cpp
+++ module_06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 19:14:34 by rde-mour          #+#    #+#             */
-/*   Updated: 2024/12/11 17:45:47 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2024/12/11 20:24:27 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 #include <cctype>
 #include <cfloat>
 #include <climits>
-#include <cmath>
-#include <cstdio>
 #include <cstdlib>
-#include <cstring>
 #include <iostream>
-#include <ostream>
-#include <sstream>
-#include <string>
 #include <sys/types.h>
+
+#ifndef SCALARCONVERTER_TYPE
+enum e_type {
+	CHAR = 1,
+	INT = 2,
+	FLOAT = 4,
+	FLOAT_INF = 8,
+	DOUBLE = 16,
+	DOUBLE_INF = 32,
+	NONE = 64
+};
+#endif /* SCALARCONVERTER_TYPE */
 
 ScalarConverter::ScalarConverter(void) {
 
@@ -43,114 +49,69 @@ ScalarConverter::~ScalarConverter(void) {
 
 }
 
-static bool validatePseudoLiterals(std::string str) {
-	
-	static const std::string literals[] = {
-		"+inff", "inff", "-inff", "nanf",
-		"+inf", "inf", "-inf", "nan"
-	};
+static enum e_type findType(std::string str) {
 
-	for (size_t i = 0; i < 8; i++) {
-		if (literals[i] == str)
-			return true;
-	}
+	if (str.size() == 1 && not std::isdigit(str.at(0)))
+		return CHAR;
 
-	return false;
-}
+	if (str == "-inff" || str == "inff" || str == "+inff" || str == "nanf")
+		return FLOAT_INF;
 
-static bool validate(std::string &str, char *rest) {
+	if (str == "-inf" || str == "inf" || str == "+inf" || str == "nan")
+		return DOUBLE_INF;
 
-	if (validatePseudoLiterals(str) == true)
-		return true;
+	if (str.at(0) == '-')
+		str = str.substr(1, str.size() - 1);
 
-	if (static_cast<ssize_t>(str.find('.')) == -1
-		&& static_cast<ssize_t>(str.find('f')) >= 0)
-		return false;
+	if (static_cast<ssize_t>(str.find_first_not_of("0123456789")) == -1)
+		return INT;
 
-	if (std::strlen(rest) > 1 || (std::strlen(rest) == 1 && rest[0] != 'f'))
-		return false;
+	if (str.size() > 2 && static_cast<ssize_t>(str.find(".")) != -1
+		&& static_cast<size_t>(str.find_first_not_of("0123456789.")) == str.size() - 1
+		&& (str.find_first_of(".") == str.find_last_of("."))
+		&& str.at(str.size() - 1) == 'f')
+		return FLOAT;
 
-	return true;
-}
+	if (static_cast<ssize_t>(str.find_last_not_of("0123456789.")) == -1
+		&& (str.find_first_of(".") == str.find_last_of(".")))
+		return DOUBLE;
 
-static std::string toChar(std::string &str, double *nbr, char *c) {
-
-	if (str.size() == 1 && std::isprint(str.at(0)) && not std::isdigit(str.at(0))) {
-		*nbr = *c = static_cast<char>(str.at(0));
-
-		return "'" + std::string(1, static_cast<char>(str.at(0))) + "'";
-	}
-
-	if (*nbr >= CHAR_MIN && *nbr <= CHAR_MAX) {
-		if (not std::isprint(static_cast<int>(*nbr)))
-			return "Non displayable";
-
-		return "'" + std::string(1, static_cast<int>(*nbr)) + "'";
-	}
-
-	return "impossible";
-}
-
-static std::string toInt(std::string &str, double *nbr, char *rest, char *c) {
-
-	std::stringstream ss;
-
-	ss << static_cast<int>(*nbr);
-	if (validate(str, rest) && *nbr >= INT_MIN && *nbr <= INT_MAX)
-		return ss.str();
-
-	if (*c)
-		return ss.str();
-
-	return "impossible";
-}
-
-static std::string toFloat(std::string &str, double *nbr, char *rest, char *c) {
-
-	std::stringstream ss;
-
-	ss << static_cast<float>(*nbr);
-
-	if ((validate(str, rest) && *nbr >= -FLT_MIN && *nbr <= FLT_MAX) || *c) {
-		int i = static_cast<int>(*nbr);
-
-		return ss.str() + (((double) i == *nbr || *c) ? ".0f" : "f");
-	}
-
-	if (validatePseudoLiterals(str))
-		return ss.str() + "f";
-
-	return "impossible";
-}
-
-static std::string toDouble(std::string &str, double *nbr, char *rest, char *c) {
-
-	std::stringstream ss;
-
-	ss << static_cast<float>(*nbr);
-
-	if ((validate(str, rest) && *nbr >= -FLT_MIN && *nbr <= FLT_MAX) || *c) {
-		int i = static_cast<int>(*nbr);
-
-		return ss.str() + (((double) i == *nbr || *c) ? ".0" : "");
-	}
-
-	if (validatePseudoLiterals(str))
-		return ss.str() + "";
-
-	return "impossible";
+	return NONE;
 }
 
 void ScalarConverter::convert(std::string str) {
 
-	char *rest = NULL;
-	double nbr = std::strtod(str.c_str(), &rest);
-	char c = 0;
+	e_type type = findType(str);
 
-	std::cout
-		<< "char: " + toChar(str, &nbr, &c) + "\n"
-		<< "int: " + toInt(str,&nbr, rest, &c) + "\n"
-		<< "float: " + toFloat(str, &nbr, rest, &c) + "\n"
-		<< "double: " + toDouble(str, &nbr, rest, &c)
-		<< std::endl;
+	double nb = 0;
+	if (type & CHAR)
+		nb = static_cast<char>(str.at(0));
+	else if (type &~ NONE)
+		nb = std::strtod(str.c_str(), NULL);
+
+	if (type &~ NONE && nb >= CHAR_MIN && nb <= CHAR_MAX) {
+		if (std::isprint(static_cast<char>(nb)))
+			std::cout << "char: '" << static_cast<char>(nb) << "'" << std::endl;
+		else
+			std::cout << "char: Non displayable" << std::endl;
+	} else
+		std::cout << "int: impossible" << std::endl;
+
+	if (type &~ NONE && nb >= INT_MIN && nb <= INT_MAX)
+		std::cout << "int: " << static_cast<int>(nb) << std::endl;
+	else
+		std::cout << "int: impossible" << std::endl;
+
+	if ((type &~ NONE && nb >= -FLT_MIN && nb <= FLT_MAX) || type & FLOAT_INF) {
+		std::cout << "float: " << static_cast<float>(nb)
+			<< ((static_cast<int>(nb) == nb) ? ".0f" : "f") << std::endl;
+	} else
+		std::cout << "float: impossible" << std::endl;
+
+	if ((type &~ NONE && nb >= -DBL_MIN && nb <= DBL_MAX) || type & (FLOAT_INF | DOUBLE_INF))
+		std::cout << "dobule: " << static_cast<double>(nb) << std::endl;
+	else
+		std::cout << "double: impossible" << std::endl;
 }
+
+#undef SCALARCONVERTER_TYPE
