7f7ed845bc8886014ff3cb73fe9706bb2bc91998
Roger de Moura <rde-mour@student.42sp.org.br>
2025-02-08 12:10:47 -0300

    refactor: remove algorithm find function

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -6,23 +6,22 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 19:12:01 by rde-mour          #+#    #+#             */
-/*   Updated: 2024/12/17 19:02:07 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2025/02/08 12:08:54 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-#include <algorithm>
 #include <stdexcept>
 
 template <typename T>
 typename T::iterator easyfind(T &container, int value) {
 
-	typename T::iterator it = std::find(container.begin(), container.end(), value);
-
-	if (*it == value)
-		return it;
+	typename T::iterator it = container.begin();
+	for (; it != container.end(); it++)
+		if (*it == value)
+			return it;
 
 	throw std::runtime_error("Exception: Easyfind: Value not Found");
 }
