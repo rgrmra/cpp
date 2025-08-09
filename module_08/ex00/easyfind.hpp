9f7b15ff03d756d734a576e8f51d155d5dddfbe7
Roger de Moura <rde-mour@student.42sp.org.br>
2025-08-09 10:54:26 -0300

    style: change curly braces layouts

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 19:12:01 by rde-mour          #+#    #+#             */
-/*   Updated: 2025/02/08 12:08:54 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2025/08/09 10:49:39 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 #include <stdexcept>
 
 template <typename T>
-typename T::iterator easyfind(T &container, int value) {
-
+typename T::iterator easyfind(T &container, int value)
+{
 	typename T::iterator it = container.begin();
 	for (; it != container.end(); it++)
 		if (*it == value)
 			return it;
 
-	throw std::runtime_error("Exception: Easyfind: Value not Found");
+	throw std::runtime_error("easyfind: Value not Found");
 }
 
 #endif /* EASYFIND_HPP */
--- module_08/ex00/main.cpp
+++ module_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rde-mour <rde-mour@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 19:30:13 by rde-mour          #+#    #+#             */
-/*   Updated: 2024/12/17 18:56:30 by rde-mour         ###   ########.org.br   */
+/*   Updated: 2025/08/09 10:51:52 by rde-mour         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,15 @@ int main(void) {
 	std::cout << "VECTOR TEST" << std::endl;
 	std::vector<int> vec;
 
-	for (std::size_t i = 1; i < 11; i++) {
+	for (std::size_t i = 1; i < 11; i++)
 		vec.push_back(i);
-	}
 
 	try {
 		std::vector<int>::iterator it;
 
 		for (std::size_t i = 1; i < 11; i++) {
 			it = easyfind(vec, i * 2);
-			std::cout << "Easyfind finds: " << *it << std::endl;
+			std::cout << "easyfind finds: " << *it << std::endl;
 		}
 	} catch (std::exception &exception) {
 		std::cerr << exception.what() << std::endl;
@@ -41,16 +40,15 @@ int main(void) {
 	std::cout << "\nDEQUE TEST" << std::endl;
 	std::deque<int> deq;
 
-	for (std::size_t i = 1; i < 11; i++) {
+	for (std::size_t i = 1; i < 11; i++)
 		deq.push_back(i);
-	}
 
 	try {
 		std::deque<int>::iterator it;
 
 		for (std::size_t i = 1; i < 11; i++) {
 			it = easyfind(deq, i * 3);
-			std::cout << "Easyfind finds: " << *it << std::endl;
+			std::cout << "easyfind finds: " << *it << std::endl;
 		}
 	} catch (std::exception &exception) {
 		std::cerr << exception.what() << std::endl;
@@ -59,16 +57,15 @@ int main(void) {
 	std::cout << "\nLIST TEST" << std::endl;
 	std::list<int> lst;
 
-	for (std::size_t i = 1; i < 11; i++) {
+	for (std::size_t i = 1; i < 11; i++)
 		lst.push_back(i);
-	}
 
 	try {
 		std::list<int>::iterator it;
 
 		for (std::size_t i = 1; i < 11; i++) {
 			it = easyfind(lst, i * 4);
-			std::cout << "Easyfind finds: " << *it << std::endl;
+			std::cout << "easyfind finds: " << *it << std::endl;
 		}
 	} catch (std::exception &exception) {
 		std::cerr << exception.what() << std::endl;
